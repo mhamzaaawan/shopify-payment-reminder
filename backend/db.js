f190feac82e5bca35f95776ae6d34ebba4668ec9
mhamzaaawan <157576800+mhamzaaawan@users.noreply.github.com>
2025-11-15 15:38:19 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 // backend/db.js - COMPLETELY FIXED VERSION
 require('dotenv').config();
-const sqlite3 = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 const dbPath = process.env.DB_PATH || path.join(__dirname, 'reminders.db');
